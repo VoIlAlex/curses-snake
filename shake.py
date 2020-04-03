ca839b996ca947cae9a00b526b3a6b0167e2eff5
VoIlAlex <ilya.vouk@gmail.com>
2020-04-04 01:18:50 +0300

    Move curses utils to separate module

--- shake.py
+++ shake.py
@@ -1,23 +1,7 @@
 import curses
 import random
 import time
-
-
-def curses_main_method(fn):
-    def method(self):
-        def bound_main_method(stdscr):
-            self.stdscr = stdscr
-            fn(self)
-        curses.wrapper(bound_main_method)
-    return method
-
-
-def curses_application(cls):
-    if not hasattr(cls, 'start'):
-        raise AttributeError("Curses main class should have 'start' method")
-    wrapped_method = curses_main_method(getattr(cls, 'start'))
-    setattr(cls, 'start', wrapped_method)
-    return cls
+from cursy import curses_application
 
 
 class ShakeBlock:
