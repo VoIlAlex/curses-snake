400c2a43af7fe43a4b795a21aa1d695e240b47c1
VoIlAlex <ilya.vouk@gmail.com>
2020-04-04 01:19:07 +0300

    Add game quiting and reseting ability

--- shake.py
+++ shake.py
@@ -30,6 +30,21 @@ class ShakeApplication:
         self.blocks = []
         self.apple = None
 
+        # Game over stuff
+        self.current_color_idx = 0
+        self.colors_number = 0
+        self.is_playing = False
+
+    def __reset_game(self):
+        self.length = 1
+        self.blocks = [ShakeBlock(self.x, self.y)]
+        self.direction = 'r'
+        self.x = 0
+        self.y = 0
+        self.delay = 1
+        self.apple = None
+        self.current_color_idx = 0
+
     def __update_blocks(self):
         for i, block in enumerate(self.blocks):
             if block.life == self.length:
@@ -57,6 +72,17 @@ class ShakeApplication:
     def __draw_apple(self):
         self.stdscr.addstr(self.apple.y, self.apple.x, self.apple_avatar)
 
+    def __draw_game_over(self):
+        height, width = self.stdscr.getmaxyx()
+        game_over = 'GAME OVER'
+        self.stdscr.addstr(height // 2, width // 2 -
+                           len(game_over),
+                           game_over,
+                           curses.A_BOLD | curses.color_pair(self.current_color_idx))
+        self.current_color_idx = (
+            self.current_color_idx + 1
+        ) % self.colors_number
+
     def __update_direction(self, ch):
         if ch != -1:
             ch = chr(ch)
@@ -69,17 +95,31 @@ class ShakeApplication:
             elif ch == 'k' and self.direction != 'd':
                 self.direction = 'u'
 
+    def __init_color(self, foreground, background=curses.COLOR_BLACK):
+        curses.init_pair(self.colors_number, foreground, background)
+        self.colors_number += 1
+
+    def __setup_curses(self):
+        curses.start_color()
+        curses.use_default_colors()
+        self.__init_color(curses.COLOR_RED)
+        self.__init_color(curses.COLOR_GREEN)
+        self.__init_color(curses.COLOR_MAGENTA)
+        self.__init_color(curses.COLOR_YELLOW)
+
     def start(self):
+        self.is_playing = True
         # Clear screen
         stdscr = self.stdscr
         stdscr.clear()
+        self.__reset_game()
+        self.__setup_curses()
         self.__generate_apple()
 
-        self.length = 1
-        self.elements = [ShakeBlock(self.x, self.y)]
-
         curses.halfdelay(self.delay)
+        ch = -1
         while True:
+
             height, width = stdscr.getmaxyx()
             ch = stdscr.getch()
             self.__update_direction(ch)
@@ -113,22 +153,43 @@ class ShakeApplication:
                     self.y = 0
                 else:
                     self.y += 1
+
             if self.__is_tail_touched():
-                return -1
+                self.is_playing = False
+                curses.halfdelay(5)
+                while True:
+                    self.stdscr.clear()
+                    self.__draw_game_over()
+                    ch = self.stdscr.getch()
+                    if ch != -1:
+                        break
+                self.__reset_game()
+                self.__generate_apple()
+                continue
             self.blocks.append(ShakeBlock(self.x, self.y))
             stdscr.clear()
+
+            # Quit the game
+            if ch != -1 and chr(ch) == 'q':
+                return
 
             if self.x == self.apple.x and self.y == self.apple.y:
                 self.length += 1
                 self.__generate_apple()
             self.__update_blocks()
-            self.__draw_apple()
+            try:
+                self.__draw_apple()
+            except:
+                self.__generate_apple()
+            finally:
+                self.__draw_apple()
             self.__draw_blocks()
             # stdscr.addstr(self.y, self.x, self.character_avatar)
             stdscr.refresh()
 
         stdscr.refresh()
         stdscr.getkey()
+        self.is_playing = False
 
 
 if __name__ == "__main__":
